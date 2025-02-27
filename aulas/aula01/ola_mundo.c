d9fb7495086edaff35bc29cd77972f478dbc5f72
Leonardo Alencar <webmaster@leonardoti.com.br>
2025-02-26 21:41:52 -0300

    codigos da aula01

---
+++ aulas/aula01/ola_mundo.c
@@ -0,0 +1,11 @@
+/*
+    Aula01 - Estrutura basica de
+    um programa em C
+*/
+#include <stdio.h> //inclusao de biblioteca
+
+// funcao principal que inicia o programa
+int main() {
+    printf("Quem és tu?\n");
+    return 0;
+}

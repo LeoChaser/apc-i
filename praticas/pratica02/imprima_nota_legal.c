39f6cf2e3c5a7e797f705d714390fc16d951249c
LeoChaser <webmaster@leonardoti.com.br>
2025-03-25 11:33:43 -0300

    Adicionando arquivos da pratica02

--- praticas/pratica02/imprima_nota_legal.c
+++ praticas/pratica02/imprima_nota_legal.c
@@ -1,7 +1,15 @@
 #include <stdio.h>
 
 int main() {
-	printf("");
+	printf("==================================\n");
+	printf("      N O T A    L E G A L\n");
+	printf("==================================\n");
+	printf("%-15s\t%s\t%s\n", "Produto", "Quant.", "Valor Unit");
+	printf("%-15s\t%03i\t     %4.2f\n", "Camiseta", 2, 39.99);
+	printf("%-15s\t%03i\t     %4.2f\n", "Calca", 1, 89.90);
+	printf("%-15s\t%03i\t     %4.2f\n","Meia Social", 3, 19.99);
+	printf("==================================\n");
+	printf("%-15s\t\t    %4.2f\n", "Total:", 229.85);
 
 	return 0;
 }

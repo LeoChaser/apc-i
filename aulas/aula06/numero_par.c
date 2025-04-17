7ada20252d8ce7290f761b07b3d60bbbc3eb5f6f
Leonardo Alencar <webmaster@leonardoti.com.br>
2025-04-16 21:56:58 -0300

    Adicionando aula06

---
+++ aulas/aula06/numero_par.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main(){
+    int numero = 0;
+    
+    printf("Entre com um numero inteiro: ");
+    scanf("%i", &numero);
+
+    int numero_eh_divisivel_por_2 = numero % 2 == 0;
+
+    if (numero_eh_divisivel_por_2) {
+        printf("O numero %i e par!\n", numero);
+    } else {
+        printf("O numero %i e impar!\n", numero);
+    }
+    // if (!numero_eh_divisivel_por_2) {
+    //     printf("O numero %i e impar!\n", numero);
+    // }
+
+    return 0;
+}

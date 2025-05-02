4126e86001acbb43131fd5c66fa81e914ff91e72
Leonardo Alencar <webmaster@leonardoti.com.br>
2025-05-02 01:54:19 -0300

    várias correções de bugs, e finalização do modo normal, falta o modo desafio

--- trabalho/forca.c
+++ trabalho/forca.c
@@ -2,7 +2,7 @@
 #include <time.h> // para gerar o dia da morte e para o srand
 #include <stdio.h>
 #include <conio.h> // para uso do clrscr no Windows
-#include <string.h> // para uso do strcmp
+#include <string.h> // para uso do strcmp e strlen
 #include <stdlib.h> // para uso do srand
 #else
 #include <time.h>
@@ -12,19 +12,19 @@
 #define clrscr() printf("\e[1;1H\e[2J")
 #endif
 
-#define RAND_MAX 4
-
 int main() {
     srand(time(NULL));
 
-    short int venceu = 0;
     char opcao_sair = '0';
     char dificuldade = 'm';
-    short int desafio = 0;
     char quebra = '\n';
     char *palavra = "palavrateste";
     char *dica = "dicateste";
+    short int fim = 0;
+    short int desafio = 0;
     short int modo_desafio_ativo = 0;
+    char *mostrar_tema = "temateste";
+    char *mostrar_dificuldade = "dificuldadeteste";
 
     char *animais[3][5] = {
         "gato", "vaca", "anta", "iguana", "abelha",
@@ -67,10 +67,15 @@ int main() {
     
     short int menu_principal = 1;
     short int menu_dificuldade = 0;
+    short int menu_tema = 0;
     while (menu_principal) {
+        short int venceu = 0;
+        short int vivo = 1;
         short int jogar = 0;
         short int resultado_menu_principal = 0;
         short int resultado_sair = 0;
+        char nome[20] = "nometeste";
+        int dia = 99, mes = 99, ano = 9999;
         if (menu_dificuldade == 0) {
             do {
                 clrscr();
@@ -109,26 +114,24 @@ int main() {
             } while (resultado_menu_principal != 2 || quebra != '\n');
         }
         short int menu_nome = 1;
+        short int resultado_menu_novamente = 0;
         while (jogar) {
-            short int vivo = 1;
-            if (menu_nome == 1) {
+            if (vivo == 1 && menu_nome == 1) {
                 menu_nome = 0;
                 clrscr();
                 printf("====================================== FORCA ======================================\n");
-                char nome[20] = "nometeste";
                 do {
                     printf("Digite seu primeiro nome:\n");
                     scanf("%[^\n]s", nome);
                     getchar();
                 } while (strcmp(nome, "nometeste") == 0); // caracteres especiais?
                 printf("Bem-vindo, %s!\n", nome);
-                int dia = 99, mes = 99, ano = 9999;
                 do {
                     printf("Que dia voce nasceu? Digite dois digitos: \n");
                     scanf("%i", &dia);
                     getchar();
                 } while (dia < 1 || dia > 31);
-                do{
+                do {
                     printf("Que mes voce nasceu? Digite dois digitos: \n");
                     scanf("%i", &mes);
                     getchar();
@@ -138,62 +141,71 @@ int main() {
                     scanf("%i", &ano);
                     getchar();
                 } while (ano < 1111 || ano > 2025);
-                menu_dificuldade = 1;
-                if (vivo == 0 && menu_principal == 0 || menu_dificuldade == 0){
-                    clrscr();
-                    printf("====================================== FORCA ======================================\n");
-                    printf("                                 _____  _______\n");
-                    printf("                                <     `/       |\n");
-                    printf("                                 >            (\n");
-                    printf("                                |    _     _   |\n");
-                    printf("                                |   |_) | |_)  |\n");
-                    printf("                                |   | \\ | |    |\n");
-                    printf("                    ___.______%%_|              |_______  _____\n");
-                    printf("                  _/                                   \\|     |\n");
-                    printf("                 |                   %s\n", nome);
-                    printf("                 |_____.-.______                ____/|________|\n");
-                    // testar!
-                    if (dia < 10 && mes < 10) {
-                        printf("                                | * 0%i/0%i/%i |\n", dia, mes, ano);
-                    } else if (dia < 10 && mes > 9) {
-                        printf("                                | * 0%i/%i/%i |\n", dia, mes, ano);
-                    } else if (dia > 9 && mes < 10) {
-                        printf("                                | * %i/0%i/%i |\n", dia, mes, ano);
-                    } else {
-                        printf("                                | * %i/%i/%i |\n", dia, mes, ano);
-                    }
-                    time_t t = time(NULL);
-                    struct tm tm = *localtime(&t);
-                    printf("                                | + %02d/%02d/%d |\n", tm.tm_mday, tm.tm_mon + 1, tm.tm_year + 1900);
-                    printf("                                |              |\n");
-                    printf("                                |   _          <\n");
-                    printf("                                |__/           |\n");
-                    printf("                                 / `--.        |\n");
-                    printf("                               %%|              |%%\n");
-                    printf("                            |/.%%|            -< @%%%%%%\n");
-                    printf("                            `%%`@|     v        |@@%%%%@%%%%    - mfj\n");
-                    printf("                          .%%%%%%@@@|%%    |    %%%%%%%%%%%% @@@%%%%%%%%%%%%@%%%%%%%%%%%%\n");
-                    printf("                     _.%%%%%%%%%%%%@@@@@@%%%%_/%%\\_%%@@%%%%%%%%@@@@@@@%%%%%%%%%%%%%%%%%%%%%%%%%%%%\n");
+            }
+            if (venceu == 0 && vivo == 0 && fim == 1) { //  && venceu == 0 && modo_desafio_ativo == 0
+                clrscr();
+                printf("====================================== FORCA ======================================\n");
+                printf("                                 _____  _______\n");
+                printf("                                <     `/       |\n");
+                printf("                                 >            (\n");
+                printf("                                |    _     _   |\n");
+                printf("                                |   |_) | |_)  |\n");
+                printf("                                |   | \\ | |    |\n");
+                printf("                    ___.______%%_|              |_______  _____\n");
+                printf("                  _/                                   \\|     |\n");
+                printf("                 |                   %s\n", nome);
+                printf("                 |_____.-.______                ____/|________|\n");
+                if (dia < 10 && mes < 10) {
+                    printf("                                | * 0%i/0%i/%i |\n", dia, mes, ano);
+                } else if (dia < 10 && mes > 9) {
+                    printf("                                | * 0%i/%i/%i |\n", dia, mes, ano);
+                } else if (dia > 9 && mes < 10) {
+                    printf("                                | * %i/0%i/%i |\n", dia, mes, ano);
+                } else {
+                    printf("                                | * %i/%i/%i |\n", dia, mes, ano);
+                }
+                time_t t = time(NULL);
+                struct tm tm = *localtime(&t);
+                printf("                                | + %02d/%02d/%d |\n", tm.tm_mday, tm.tm_mon + 1, tm.tm_year + 1900);
+                printf("                                |              |\n");
+                printf("                                |   _          <\n");
+                printf("                                |__/           |\n");
+                printf("                                 / `--.        |\n");
+                printf("                               %%|              |%%\n");
+                printf("                            |/.%%|            -< @%%%%%%\n");
+                printf("                            `%%`@|     v        |@@%%%%@%%%%    - mfj\n");
+                printf("                          .%%%%%%@@@|%%    |    %%%%%%%%%%%% @@@%%%%%%%%%%%%@%%%%%%%%%%%%\n");
+                printf("                     _.%%%%%%%%%%%%@@@@@@%%%%_/%%\\_%%@@%%%%%%%%@@@@@@@%%%%%%%%%%%%%%%%%%%%%%%%%%%%\n");
 
-                    printf("Deseja jogar novamente? S/N\n");
-                    scanf("%c", &opcao_sair);
-                    getchar();
+                printf("Deseja jogar novamente? S/N\n");
+                resultado_menu_novamente = scanf("%c%c", &opcao_sair, &quebra);
+                do {
                     if (opcao_sair == 'n' || opcao_sair == 'N') {
                         clrscr();
                         printf("Obrigado, encerrando...\n");
                         return 0;
-                    } else {
+                    } else if (opcao_sair == 's' || opcao_sair == 'S') {
                         jogar = 0;
                         menu_principal = 1; // testar
+                        menu_dificuldade = 0;
+                        menu_tema = 0;
+                        modo_desafio_ativo = 0;
+                    } else {
+                        printf("Por gentileza, escolha uma dificuldade valida.\n");
+                        printf("Pressione ENTER para continuar.\n");
+                        getchar();
                     }
-                }
+                } while (resultado_menu_novamente != 2 && quebra != '\n');
             }
-
+            menu_dificuldade = 1;
             short int max_erros = 4;
-            char *mostrar_dificuldade = "dificuldadeteste";
             short int posicao_dificuldade = 1;
             short int resultado_menu_dificuldade = 0; // testar
             do {
+                if (modo_desafio_ativo){
+                    menu_dificuldade = 0;
+                    break;
+                }
                 do {
                     clrscr();
                     printf("====================================== FORCA ======================================\n");
@@ -244,10 +256,14 @@ int main() {
                 } while (resultado_menu_dificuldade != 2 || quebra != '\n');
             } while (menu_dificuldade);
 
-            short int menu_tema = 1;
-            char *mostrar_tema = "temateste";
+            menu_tema = 1;
             short int resultado_menu_tema = 0;
+            char tema = '1';
             do {
+                if (modo_desafio_ativo) {
+                    menu_tema = 0;
+                    break;
+                }
                 do {
                     clrscr();
                     printf("====================================== FORCA ======================================\n");
@@ -256,9 +272,9 @@ int main() {
                     printf("f para frutos/frutas/pseudofrutos.\n");
                     printf("c para caes.\n");
                     printf("0 para encerrar.\n");
-                    char tema = '1';
+                    tema = '1';
+                    int aleatorio = rand() % 5;
                     resultado_menu_tema = scanf("%c%c", &tema, &quebra);
-                    short int aleatorio = rand();
                     switch (tema) {
                         case 'a':
                             palavra = animais[posicao_dificuldade][aleatorio];
@@ -301,18 +317,18 @@ int main() {
                 } while (resultado_menu_tema != 2 || quebra != '\n');
             } while (menu_tema);
             if (modo_desafio_ativo) {
-                if (strcmp(mostrar_tema, "animais")) {
+                if (tema == 'a') {
                     palavra = desafio_animais;
                     dica = dica_desafio_animais;
-                } else if (strcmp(mostrar_tema, "caes")) {
+                } else if (tema == 'c') {
                     palavra = desafio_caes;
                     dica = dica_desafio_caes;
-                } else {
+                } else if (tema == 'f') {
                     palavra = desafio_frutas;
                     dica = dica_desafio_frutas;
                 }
             }
-            int tamanho_palavra = sizeof(palavra)/sizeof(palavra[0]);
+            int tamanho_palavra = strlen(palavra);
             short int acertou = 0;
 
             char chutes_certos[tamanho_palavra];
@@ -323,12 +339,17 @@ int main() {
             short int erros = 0;
             short int quant_chutes = 0;
             clrscr();
+            vivo = 1;
             while (vivo) {
                 char chute;
                 char chutes[max_erros];
                 if (acertos < tamanho_palavra) {
                     printf("====================================== FORCA ======================================\n");
-                    printf("Dificuldade: %s; Tema: %s.\n", mostrar_dificuldade, mostrar_tema);
+                    if (modo_desafio_ativo) {
+                        printf("Dificuldade: DESAFIO; Tema: %s.\n\n\n", mostrar_tema);
+                    } else {
+                        printf("Dificuldade: %s; Tema: %s.\n\n\n", mostrar_dificuldade, mostrar_tema);
+                    }
                     printf("Chute uma letra 'a-z':\n");
                     printf("0 para encerrar.\n");
                     printf("Palavra:\n");
@@ -342,6 +363,11 @@ int main() {
                     }
                     printf("\n\n");
                     printf("Chutes restantes: %i!\n\n", max_erros - erros);
+                    if (max_erros - erros <= 2) {
+                        printf("Dica: %s!\n", dica);
+                    } else {
+                        printf("\n");
+                    }
                     acertou = 0;
 
                     printf("Chutes feitos:\n");
@@ -369,6 +395,11 @@ int main() {
                         printf("Pressione ENTER para continuar!\n");
                         getchar();
                         vivo = 0;
+                        venceu = 0;
+                        fim = 1;
+                        menu_dificuldade = 0;
+                        menu_tema = 0;
+                        break;
                     } else {
                         printf("______\n");
                         printf("|    :\n");
@@ -413,7 +444,11 @@ int main() {
                     }
                 } else {
                     printf("====================================== FORCA ======================================\n");
-                    printf("Dificuldade: %s; Tema: %s.\n\n\n", mostrar_dificuldade, mostrar_tema);
+                    if (modo_desafio_ativo) {
+                        printf("Dificuldade: DESAFIO; Tema: %s.\n\n\n", mostrar_tema);
+                    } else {
+                        printf("Dificuldade: %s; Tema: %s.\n\n\n", mostrar_dificuldade, mostrar_tema);
+                    }
                     printf("Palavra:\n");
                     short int i;
                     for (i = 0; i < tamanho_palavra; i += 1) {
@@ -464,26 +499,25 @@ int main() {
                     printf("                      |                     / \\     |\n");
                     printf("                       =============================\n");
 
-                    if (dificuldade == 'd') {
+                    if (dificuldade == 'd' && modo_desafio_ativo == 0) {
                         short int menu_desafio = 1;
                         short int resultado_menu_desafio = 0;
                         printf("Voce desbloqueou o modo desafio!\n");
                         do {
                             do {
                                 printf("Por obsequio, digite uma opcao:\n");
-                                printf("1 para jogar o modo desafio!\n");
+                                printf("d para jogar o modo desafio!\n");
                                 printf("0 para sair...\n");
-                                scanf("%c", desafio);
-                                getchar();
+                                resultado_menu_desafio = scanf("%c%c", &desafio, &quebra);
                                 switch (desafio) {
-                                    case '1':
-                                        max_erros = 4;
+                                    case 'd':
                                         modo_desafio_ativo = 1;
                                         menu_desafio = 0;
+                                        menu_nome = 0;
+                                        vivo = 0;
                                         break;
                                     case '0':
                                         printf("Tem certeza que deseja encerrar? S/N\n");
-                                        resultado_menu_desafio = 0;
                                         do {
                                             resultado_sair = scanf("%c%c", &opcao_sair, &quebra);
                                             if (resultado_sair == 2 && quebra == '\n' && opcao_sair == 's' || opcao_sair == 'S') {
@@ -506,7 +540,6 @@ int main() {
                     } else {
                         printf("Deseja jogar novamente? S/N\n");
                         char opcao_jogar_novamente = '0';
-                        short int resultado_menu_novamente = 0;
                         do {
                             resultado_menu_novamente = scanf("%c%c", &opcao_jogar_novamente, &quebra);
                             if (opcao_jogar_novamente == 'n' || opcao_jogar_novamente == 'N') {
@@ -514,21 +547,19 @@ int main() {
                                 printf("Obrigado, encerrando...\n");
                                 return 0;
                             } else if (opcao_jogar_novamente == 's' || opcao_jogar_novamente == 'S') {
-                                venceu = 0;
                                 printf("Selecione uma opcao:\n");
                                 printf("t para trocar dificuldade e tema.\n");
                                 printf("m para menu principal (novo participante).\n");
                                 char opcao_menu_vencedor = '0';
                                 short int resultado_menu_vencedor = scanf("%c%c", &opcao_menu_vencedor, &quebra);
                                 do {
+                                    vivo = 0;
                                     switch (opcao_menu_vencedor) {
                                         case 't':
-                                            vivo = 0;
                                             menu_nome = 0;
                                             menu_dificuldade = 1;
                                             break;
                                         case 'm':
-                                            vivo = 0;
                                             jogar = 0;
                                             menu_nome = 1;
                                             menu_principal = 1;
